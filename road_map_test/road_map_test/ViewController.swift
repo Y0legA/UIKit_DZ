a8a9fdb075eae1e3b78670dfbb7ca976a2e9b6dd
Oleg <myaddub@gmail.com>
2022-09-22 01:06:05 +0300

    сделал 1,2,3,4 3 урок

---
+++ road_map_test/road_map_test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  road_map_test
+//
+//  Created by Oleg_Yakovlev on 22.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+"""
+Создать второе приложение MVC у которого на
+экране будет кнопка "начать" которая вызывает Алерт
+с сообщением "введите слово"
+6 Нужно ввести "leohl" в Алерт нажимаете "ок" отдаете
+контролеру а контролер отдаст это в модель которая
+из буквосочетания "leohl" вернет слово "hello"
+контролеру а контролер отдаст это лейблу и отобразит
+это на экране
+"""
