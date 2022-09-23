9c6857f1102b2bf4b15d506243c8ff9408c5aef2
Oleg <myaddub@gmail.com>
2022-09-23 16:23:24 +0300

    Исправил 7 замечаний

--- UIKitDZ/UIKitDZ/Games.swift
+++ UIKitDZ/UIKitDZ/Games.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Games.swift
 //  UIKitDZ
 //
 //  Created by Oleg_Yakovlev on 21.09.22.
@@ -7,70 +7,81 @@
 
 import UIKit
 
-/// Cтартовый контроллер
-class ViewController: UIViewController {
+/// Экран ввода имени, расчета суммы, с игрой "Угадай число"
+class Games: UIViewController {
     
     private var nameLabel = UILabel()
     private var resultSumLabel = UILabel()
-    
     private var guessNumberLabel = UILabel()
     private var guessNumberTextField = UITextField()
     private var tryGuessNumberButton = UIButton()
     private var guessNumberStartGameButton = UIButton()
-    private var randomNumber = UInt8()
     private var sumButton = UIButton()
+    private var randomNumber = Int()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        
     }
     
     /// Настройка UI
     private func configureUI() {
-        view.backgroundColor = .cyan
+        configureNameLabel()
+        configureGuestNumberTextField()
+        confifureGuessNumberStartGameButton()
+        configureTryGuessNumberButton()
+        configureResultSumNumber()
+        configureNumberButton()
+    }
+    
+    private func configureNameLabel() {
         view.addSubview(self.nameLabel)
         self.nameLabel.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         self.nameLabel.textAlignment = .center
         self.nameLabel.frame = CGRect(x: 60, y: 50, width: 240, height: 30)
-        
-        self.view.addSubview(self.guessNumberLabel)
-        self.guessNumberLabel.frame = CGRect(x: 40, y: 150, width: 170, height: 30)
-        self.guessNumberLabel.text = "Guess the number:)"
-        self.guessNumberLabel.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
-        
+    }
+    
+    private func configureGuestNumberTextField() {
         self.view.addSubview(self.guessNumberTextField)
         self.guessNumberTextField.text = "1"
         self.guessNumberTextField.frame = CGRect(x: 230, y: 150, width: 80, height: 30)
         self.guessNumberTextField.textAlignment = .center
         self.guessNumberTextField.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
-        
+    }
+    
+    private func confifureGuessNumberStartGameButton() {
         self.view.addSubview(self.guessNumberStartGameButton)
         self.guessNumberStartGameButton.frame = CGRect(x: 60, y: 200, width: 120, height: 30)
         self.guessNumberStartGameButton.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         self.guessNumberStartGameButton.setTitle("start game", for: .normal)
         self.guessNumberStartGameButton.addTarget(self, action: #selector(startGame), for: .touchUpInside)
         self.guessNumberStartGameButton.layer.cornerRadius = 15
-        
+    }
+    
+    private func configureTryGuessNumberButton() {
         self.view.addSubview(self.tryGuessNumberButton)
         self.tryGuessNumberButton.frame = CGRect(x: 220, y: 200, width: 80, height: 30)
         self.tryGuessNumberButton.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         self.tryGuessNumberButton.setTitle("try", for: .normal)
         self.tryGuessNumberButton.addTarget(self, action: #selector(tryGuess), for: .touchUpInside)
         self.tryGuessNumberButton.layer.cornerRadius = 15
-        
+    }
+    
+    private func configureResultSumNumber() {
         self.view.addSubview(self.resultSumLabel)
         self.resultSumLabel.frame = CGRect(x: 120, y: 500, width: 150, height: 30)
-        
+    }
+    
+    private func configureNumberButton() {
         self.view.addSubview(self.sumButton)
         self.sumButton.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
         self.sumButton.setTitle("calculate", for: .normal)
         self.sumButton.layer.cornerRadius = 15
-        self.sumButton.addTarget(self, action: #selector(calcAlert), for: .touchUpInside)
+        self.sumButton.addTarget(self, action: #selector(alertNumberingNumbers), for: .touchUpInside)
         self.sumButton.frame = CGRect(x: 135, y: 460, width: 100, height: 30)
     }
     
-    /// Алерт при запуске приложения
+    /// Приглашение ввода ФИО при запуске приложения
     private func startAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Show", style: .default) { _ in
@@ -83,8 +94,8 @@ class ViewController: UIViewController {
         self.present(alertController, animated: true)
     }
     
-    /// Алерт для ввода чисел
-    @objc func calcAlert() {
+    /// Метод для обработки вводимых чисел
+    @objc func alertNumberingNumbers() {
         let alertController = UIAlertController(title: "Sum", message: "Enter the numbers...",
                                                 preferredStyle: .alert)
         let action = UIAlertAction(title: "Calculate", style: .default) { _ in
@@ -107,7 +118,7 @@ class ViewController: UIViewController {
     private func guessNumber() {
         self.guessNumberLabel.text = "GUESS NUMBER"
         
-        guard let userNumber = UInt8(self.guessNumberTextField.text ?? "") else {
+        guard let userNumber = Int(self.guessNumberTextField.text ?? "") else {
             return self.guessNumberTextField.text = "no correct enter"
         }
         
@@ -128,7 +139,7 @@ class ViewController: UIViewController {
     
     /// Запуск игры "Угадай число"
     @objc func startGame() {
-        self.randomNumber = UInt8.random(in: 1...250)
+        self.randomNumber = Int.random(in: 1...250)
         self.guessNumberStartGameButton.isUserInteractionEnabled = false
         self.guessNumberStartGameButton.isHidden = true
     }
@@ -136,10 +147,10 @@ class ViewController: UIViewController {
     /// Попытка угадать число
     @objc func tryGuess() {
         self.guessNumber()
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         self.startAlert(title: "Hello!", message: "Enter your SNL...")
     }
 }
