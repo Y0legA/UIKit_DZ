a1fb2f2a47b47119e6800683c2dcd883712b6bdd
Oleg <myaddub@gmail.com>
2022-10-01 20:19:33 +0300

    Сделал 11 урок * Разбил сториборды

---
+++ UIKitDZ/UIKitDZ/TimerViewController.swift
@@ -0,0 +1,62 @@
+//
+//  TimerViewController.swift
+//  UIKitDZ
+//
+//  Created by Oleg_Yakovlev on 1.10.22.
+//
+
+import UIKit
+/// Экран таймера
+class TimerViewController: UIViewController {
+    @IBOutlet weak var timerPickerView: UIPickerView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+
+    // MARK: - lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Private Methods
+    private func configureUI() {
+        timerPickerView.delegate = self
+        timerPickerView.dataSource = self
+        cancelButton.layer.borderColor = #colorLiteral(red: 0.129522278, green: 0.129522278, blue: 0.129522278, alpha: 1)
+        startButton.layer.borderColor = #colorLiteral(red: 0.04249797016, green: 0.1647131443, blue: 0.06017308682, alpha: 1)
+    }
+}
+
+// MARK: - UIPickerViewDelegate, UIPickerViewDataSource
+extension TimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        3
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 24
+        case 1:
+            return 60
+        case 2:
+            return 60
+        default:
+            return 0
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(row)"
+        case 1:
+            return "\(row)"
+        case 2:
+            return "\(row)"
+        default:
+            return ""
+        }
+    }
+}
